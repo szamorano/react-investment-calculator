d7a23b5ae998bccdbefc5048973e2ad93ccdedb6
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-06-14 17:07:21 -0400

    Added the UserInput component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Header from "./components/Header"
-import UserInput from "./components/UserInput"
 
 function App() {
   return (
-    // <Header />
-    <UserInput/>
+    <Header />
   )
 }
 
